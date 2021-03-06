eb7e180b4b3986c9cb85c3918da257c989bde026
mr.huo <455801665@qq.com>
2021-03-06 15:45:09 +0800

    topnav样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
         <div class="topnav">
-            <div class="log"></div>
-            <div class="menu"></div>
+            <div class="logo">Logo</div>
+            <ul class="menu">
+                <li>菜单1</li>
+                <li>菜单2</li>
+            </ul>
         </div>
         <div class="banner">
             <h1>UI</h1>
@@ -16,7 +19,22 @@
 </template>
 <style lang="scss" scoped>
     .topnav {
-        border: 1px solid red;
+        background:#c67085;
+        display: flex;
+        padding: 16px;
+        >.logo{
+            max-width: 6em;
+            margin-right: auto;
+        }
+        >.menu{
+            display: flex;
+            white-space: nowrap;
+            flex-wrap: nowrap;
+            >li{
+                margin:0 1em;
+            }
+        }
+
     }
 
 </style>

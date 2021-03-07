7244a279d2fd3d74b93317b93cdae45534974784
mr.huo <455801665@qq.com>
2021-03-07 17:48:33 +0800

    aside的css1

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -6,7 +6,23 @@
             <div class="menu"></div>
         </div>
         <div class="content">
-            <aside>边栏</aside>
+            <aside>
+                <h2>组件列表</h2>
+                <ul>
+                    <li>
+                        <router-link to="/doc/switch">Switch 组件</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/button">Button 组件</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/dialog">Dialog 组件</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/tab">Tab 组件</router-link>
+                    </li>
+                </ul>
+            </aside>
             <main>内容</main>
         </div>
     </div>
@@ -19,3 +35,22 @@
       }
     }
 </script>
+<style lang="scss" scoped>
+    aside{
+        background: pink;
+        width: 150px;
+        padding: 16px;
+        position: fixed;
+        top: 0;
+        left: 0;
+        >h2{
+            margin-bottom: 4px;
+        }
+        >ol{
+            >li{
+                padding: 4px 0;
+            }
+        }
+    }
+
+</style>
